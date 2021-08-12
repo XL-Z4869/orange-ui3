395f3310f763005ae57cf7876e78cd86b477b894
ZLM <2113743321@qq.com>
2021-08-12 23:37:14 +0800

    初始化vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>hi</div>
-  <Frank/>
+   <div>导航栏|
+     <router-link to="/">Frank</router-link>
+     <router-link to="/xxx">Frank2</router-link>
+     <hr/>
+     <router-view />
 </template>
 
 <script>
  
-import Frank from './components/Frank.vue'
+ 
 export default {
   name: 'App',
-  components: {
-    Frank
-  }
+ 
 }
 </script>
