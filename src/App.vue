bcb8ec87a428812e1a187ee277d5391f73b29777
ZLM <2113743321@qq.com>
2021-08-13 12:59:09 +0800

    实现aside栏切换

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
+  <div>
      <router-view />
+  </div>
 </template>
 
 <script>
  
+import { provide,ref } from 'vue'
+ 
+ 
  
 export default {
   name: 'App',
- 
+  setup(){
+    const asideVisible=ref(false)
+    provide('asideVisible',asideVisible)
+  }
 }
 </script>
