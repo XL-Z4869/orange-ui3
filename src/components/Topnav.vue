afb0cf2afa05bd148dae46b5a47ab25cbdd2f1b8
ZLM <2113743321@qq.com>
2021-08-13 13:27:16 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleAside">LOGO</div>
+        <div class="logo" >LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
-        <span class="toggleAside"></span>
+        <span class="toggleAside" @click="toggleAside"></span>
     </div>
 </template>
 <script lang="ts">
@@ -51,10 +51,12 @@ export default {
         left: 16px;
         top: 50%;
         transform: translateY(-50%);
+        display: none;
     }
     @media (max-width:500px) {
         >.menu{display: none;}
         >.logo{margin:0 auto}
+        >.toggleAside{display: inline-block;}
     }
 }
 </style>
