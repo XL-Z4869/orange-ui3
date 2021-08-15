16af0b4b49108c7987d47bd67c7bb92b1da25534
ZLM <2113743321@qq.com>
2021-08-15 22:51:40 +0800

    创建Tabs和tab组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,5 +2,18 @@
   <div>
       Tabs实例
   </div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tab.vue'
+import Tab from '../lib/Tabs.vue'
+export default {
+  components:{Tab,Tabs}
+}
+</script>
  
