c88fc98253718d9b1c5f052e70c0c96497433b64
ZLM <2113743321@qq.com>
2021-08-15 15:47:18 +0800

    自定义title和content

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
   <div class="orange-dialog-wrapper">
       <div class="orange-dialog">
       <header>
-          标题
+          <slot name="title" />
           <span @click="close" class="orange-dialog-close"></span>
       </header>
       <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content" />
       </main>
       <footer>
           <Button level="main" @click="ok">OK</Button>
