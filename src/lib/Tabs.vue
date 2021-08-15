16e7c52028b68f8a5bc798e250e0ac4157b12a3e
ZLM <2113743321@qq.com>
2021-08-15 23:40:49 +0800

    Tabs css1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,11 @@
 <template>
-<div>
-  Tabs 组件
-  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-  <component v-for="(c,index) in defaults" :is="c" :key="index" />
+<div class="orange-tabs">
+<div class="orange-tabs-nav">
+  <div class="orange-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+</div>
+<div class="orange-tabs-content"> 
+  <component class="orange-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+</div>
 </div>
 </template>
 
@@ -26,3 +29,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.orange-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
