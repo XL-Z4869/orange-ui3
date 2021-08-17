e0b440a948e28f7db390eda55acce300654933dc
ZLM <2113743321@qq.com>
2021-08-17 17:36:28 +0800

    展示Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,34 +1,51 @@
-<template>
-    <div class="demo">
-        <h2>{{component.__sourceCodeTitle}}</h2>
-        <div class="demo-component">
-            <component :is="component" /> 
-        </div>
-        <div class="demo-actions">
-            <Button>查看代码</Button>
-        </div>
-        <div class="demo-code">
-             <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
-        </div>
-    </div>
+ <template>
+<div class="demo">
+  <h2>{{component.__sourceCodeTitle}}</h2>
+  <div class="demo-component">
+    <component :is="component" />
+  </div>
+  <div class="demo-actions">
+    <Button @click="toggleCode">查看代码</Button>
+  </div>
+  <div class="demo-code" v-if="codeVisible">
+    <pre class="language-html" v-html="html" />
+  </div>
+
+</div>
 </template>
-
-<script>
+<script lang="ts">
 import Button from '../lib/Button.vue'
-import 'prismjs'
+import 'prismjs';
 import 'prismjs/themes/prism.css'
+import {
+  computed,
+  ref
+} from 'vue';
+const Prism = (window as any).Prism
 export default {
-    props:{
-        component:{Object,Button}
-    },
-    setup(){
-        return {
-            Prism
-        }
+  components: {
+    Button
+  },
+  props: {
+    component: Object
+  },
+  
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
+    })
+    const toggleCode = () => codeVisible.value = !codeVisible.value
+    const codeVisible = ref(false)
+    return {
+      Prism,
+      html,
+      codeVisible,
+      toggleCode
     }
+  }
 }
-</script>
 
+</script>
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
 .demo {
