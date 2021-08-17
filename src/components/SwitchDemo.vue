5513b33bb25a23360ee6c8d2a78ca76a3ab08649
ZLM <2113743321@qq.com>
2021-08-17 17:38:43 +0800

    删除无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,29 +7,25 @@
 </template>
 <script lang="ts">
 import { ref } from 'vue'
-import Switch from '../lib/Switch.vue'
-import Button from '../lib/Button.vue'
+ 
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
 import Demo from './Demo.vue'
-import 'prismjs'
-import 'prismjs/themes/prism.css'
 
-const Prism =(window as any).Prism
 
 console.log(Switch1Demo.__sourceCode)
 export default {
      components:{
-        Button,
+        
         Demo
        },
      setup(){
-         const bool=ref(false)
+         
          return {
-          bool,
+         
           Switch1Demo,
           Switch2Demo,
-          Prism}
+          }
      }
 }
 </script>
