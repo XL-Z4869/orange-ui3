60d578cf71eb64e1c5d7cef1a16e353a810dafdf
ZLM <2113743321@qq.com>
2021-08-17 14:31:27 +0800

    优化markdown

---
+++ src/components/Markdown.vue
@@ -0,0 +1,23 @@
+<template>
+   <article class="markdown-body" v-html="content"></article>
+</template>
+
+<script>
+export default {
+    props:{
+        path:{
+            typr:String,
+            required:true
+        }
+    },
+    setup(props){
+        const content=ref<string>(null)
+        import(props.path).then(result=>{
+            content.value=result.default
+        })
+        return {
+            content
+        }
+    }
+}
+</script>
