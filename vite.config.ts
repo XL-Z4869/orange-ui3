bd09dd49fca872b5a1e0328891a52d9592807633
ZLM <2113743321@qq.com>
2021-08-18 11:32:22 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { baseParse } from '@vue/compiler-core'
 
 
 export default {
+  base:'./',
+  assetsDir:'assets',
     plugins: [md()],
     vueCustomBlockTransforms: {
         demo: (options) => {
